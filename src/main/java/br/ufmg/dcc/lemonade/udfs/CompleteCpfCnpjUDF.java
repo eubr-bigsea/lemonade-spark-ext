b8dec302180e40c84f17243c9b1724c3df767752
ufmg.slipovetsky <samuellpvt@gmail.com>
2026-02-02 16:15:49 -0300

    Fix: fixing completeCpfCnpjUDF logic and also creating new parameter for fill char

--- src/main/java/br/ufmg/dcc/lemonade/udfs/CompleteCpfCnpjUDF.java
+++ src/main/java/br/ufmg/dcc/lemonade/udfs/CompleteCpfCnpjUDF.java
@@ -1,18 +1,17 @@
 package br.ufmg.dcc.lemonade.udfs;
 
-import org.apache.spark.sql.api.java.UDF2;
+import org.apache.spark.sql.api.java.UDF3;
 import org.apache.spark.sql.expressions.UserDefinedFunction;
 
-public class CompleteCpfCnpjUDF implements UDF2<String, String, String> {
+public class CompleteCpfCnpjUDF implements UDF3<String, String, String, String> {
 
     private int DIVISOR = 11;
 
-    private String calculate_first_digit (String number) {
+    private String calculate_first_digit(String number) {
 
-        int[] CPF_WEIGHTS = {10, 9, 8, 7, 6, 5, 4, 3, 2};
-        int[] CNPJ_WEIGHTS = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[] CPF_WEIGHTS = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] CNPJ_WEIGHTS = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
 
-        
         int sum = 0;
         int[] weights = new int[15];
 
@@ -21,14 +20,14 @@ public class CompleteCpfCnpjUDF implements UDF2<String, String, String> {
         else
             weights = CNPJ_WEIGHTS;
 
-        for (int i = 0; i  < number.length(); i++) {
+        for (int i = 0; i < number.length(); i++) {
             char digit = number.charAt(i);
             sum = (sum + Character.getNumericValue(digit) * weights[i]);
         }
 
         int rest_division = sum % DIVISOR;
 
-        if (rest_division < 2){
+        if (rest_division < 2) {
             return "0";
         }
 
@@ -38,9 +37,8 @@ public class CompleteCpfCnpjUDF implements UDF2<String, String, String> {
 
     private String calculate_second_digit(String number) {
 
-        int[] CPF_WEIGHTS = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-        int[] CNPJ_WEIGHTS = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
-
+        int[] CPF_WEIGHTS = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] CNPJ_WEIGHTS = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
 
         int sum = 0;
         int[] weights = new int[15];
@@ -50,14 +48,14 @@ public class CompleteCpfCnpjUDF implements UDF2<String, String, String> {
         else
             weights = CNPJ_WEIGHTS;
 
-        for (int i = 0; i  < number.length(); i++) {
+        for (int i = 0; i < number.length(); i++) {
             char digit = number.charAt(i);
             sum = (sum + Character.getNumericValue(digit) * weights[i]);
         }
 
         int rest_division = sum % DIVISOR;
 
-        if (rest_division < 2){
+        if (rest_division < 2) {
             return "0";
         }
 
@@ -66,37 +64,51 @@ public class CompleteCpfCnpjUDF implements UDF2<String, String, String> {
     }
 
     @Override
-    public String call(String number, String tipo_pessoa) throws Exception {
+    public String call(String number, String tipo_pessoa, String fill_string) throws Exception {
 
-      if (number == null || number.equals("NULL")){
-          return null;
-      }
+        if (number == null || number.equals("NULL")) {
+            return null;
+        }
 
-      String number_final = number;
-      String clean_number = number.replaceAll("[^0-9]", "");
+        String number_final = number;
+        String clean_number = number.replaceAll("[^0-9]", "");
+        char fillChar = (fill_string == null || fill_string.isEmpty())
+        ? '0'
+        : fill_string.charAt(0);
 
-      if (tipo_pessoa.equals("F")) {
-            if (clean_number.length() == 9){
+        if (tipo_pessoa.equals("F")) {
+            if (clean_number.length() == 9) {
                 String first_part = clean_number.substring(0, 9);
                 String first_digit = calculate_first_digit(first_part);
                 String second_part = first_part + first_digit;
                 String second_digit = calculate_second_digit(second_part);
                 number_final = second_part + second_digit;
             } else if (clean_number.length() < 9) {
-                number_final = number + ("" + (11 - number.length()));
+
+                StringBuilder sb = new StringBuilder(clean_number);
+              
+                for (int i = clean_number.length(); i < 11; i++) {
+                    sb.append(fillChar);
+                }
+                number_final = sb.toString();
             }
-      } else if (tipo_pessoa.equals("J")) {
-            if (clean_number.length() == 12){
+        } else if (tipo_pessoa.equals("J")) {
+            if (clean_number.length() == 12) {
                 String first_part = clean_number.substring(0, 12);
                 String first_digit = calculate_first_digit(first_part);
                 String second_part = first_part + first_digit;
                 String second_digit = calculate_second_digit(second_part);
                 number_final = second_part + second_digit;
             } else if (clean_number.length() < 12) {
-                number_final = number + ("" + (14 - number.length()));
+
+                StringBuilder sb = new StringBuilder(clean_number);
+                for (int i = clean_number.length(); i < 14; i++) {
+                    sb.append(fillChar);
+                }
+                number_final = sb.toString();
             }
-      }
-      return number_final;
-  }
+        }
+        return number_final;
+    }
 
 }
